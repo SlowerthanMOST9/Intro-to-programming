d6f6f8d82548312617c030a997ba78a15dea9f5a
Logan Madej <lnmadej23@gmail.com>
2020-12-02 10:48:18 -0500

    Work

---
+++ Day1/string.js
@@ -0,0 +1,16 @@
+// alert("Hello Person That Is Not Me!");
+
+var x = 5;
+var y = 10;
+var xSquared = Math.pow(x, 2);
+var ySquared = Math.pow(x, 2);
+var z = Math.sqrt(xSquared + ySquared);
+var name = "Logan";
+console.log("Your Name Is "+name)
+console.log("x+y=" + x+y);
+console.log("x+y="+ parseInt(x+y));
+console.log("x+y="+"Ninja");
+
+alert("Hello World");
+var userName = prompt("Whait Is your Username?");
+alert("Hello, "+userName);
